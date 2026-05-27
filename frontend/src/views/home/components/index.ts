17089b8cc5d44ef5a9fab00e300c1c3328a59583
Tunar Məmmədli <tunarmamadli07@gmail.com>
2026-05-27 16:25:46 +0400

    fix: update home module exports to reference refactored components correctly
    - Remove outdated imports of components that were moved to components/ subfolder
    - Add index.ts in home/components with organized exports
    - Fixes all svelte-check errors (was 4 errors, now 0)

---
+++ frontend/src/views/home/components/index.ts
@@ -0,0 +1,6 @@
+export { default as LauncherHero } from './LauncherHero.svelte';
+export { default as LauncherStatus } from './LauncherStatus.svelte';
+export { default as LauncherRecentFiles } from './LauncherRecentFiles.svelte';
+export { default as LauncherRecentNotes } from './LauncherRecentNotes.svelte';
+export { default as LauncherRecentWorkspaces } from './LauncherRecentWorkspaces.svelte';
+export { default as LauncherActivityPanel } from './LauncherActivityPanel.svelte';
--- frontend/src/views/home/index.ts
+++ frontend/src/views/home/index.ts
@@ -1,10 +1,2 @@
-/**
- * Home module exports
- * Centralizes home/launcher components
- */
-
 export { default as HomeLauncher } from './HomeLauncher.svelte';
-export { default as LauncherActivity } from './LauncherActivity.svelte';
-export { default as LauncherAdminOverview } from './LauncherAdminOverview.svelte';
-export { default as LauncherHero } from './LauncherHero.svelte';
-export { default as LauncherRecents } from './LauncherRecents.svelte';
+export * from './components/';
